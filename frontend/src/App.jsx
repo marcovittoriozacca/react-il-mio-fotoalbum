5020eafd5dd80a0cf694de945b7db868e9d4997d
marcovittoriozacca <zaccamarco01@gmail.com>
2024-06-27 10:17:12 +0200

    feat: frontend - initial router setting + homepage and 404 page handler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-
+import { Routes, Route } from "react-router-dom"
+import MainLayout from "./layouts/MainLayout"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 function App() {
   return (
     <>
+    <Routes>
+      <Route path="/" element={<MainLayout/>}>
+        {/* home page at path "/" */}
+        <Route index element={<Home/>}/>
+
+
+        {/* not found route */}
+        <Route path="*" element={<NotFound/>}/>
+      </Route>
+    </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function(){
+    return(<>
+        <section className="h-screen grid place-items-center">
+            <h1>404 - Page Not Found</h1>        
+        </section>
+    </>)
+}
